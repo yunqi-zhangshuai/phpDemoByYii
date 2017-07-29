ce4acb3e35204938f77855f3c6a3ba10a9e733e2
zhangjinshuai <zhangjinshuai668@126.com>
2017-07-29 14:39:11 +0800

    初始化框架

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=emp',
+    'username' => 'root',
+    'password' => '5574301199a',
+    'charset' => 'utf8',
+];
---
+++ models/Emp.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 云起
+ * Date: 2017-07-28
+ * Time: 11:07
+ * File: Emp.php
+ */
+
+namespace app\models;
+
+
+use yii\db\ActiveRecord;
+
+/**
+ * Class Emp
+ * @package app\models
+ * 测试数据curd
+ */
+class Emp extends ActiveRecord
+
+{
+    /**
+     * 返回当前表名
+     * @return string
+     */
+    public static function tableName()
+    {
+        return 'emp';
+    }
+
+    /**
+     * @return array
+     */
+    public function actionFind()
+    {
+        return (new \yii\db\Query())
+            ->select(['dept.dname as name', 'job', 'ename'])
+            ->from('emp')
+            ->leftJoin('dept', 'dept.deptno=emp.deptno')
+            ->all();
+
+
+    }
+}
