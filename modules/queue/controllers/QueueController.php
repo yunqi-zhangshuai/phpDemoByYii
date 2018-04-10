777009d2bc647a670e599b6f1ab1534d423c3218
zhangjinshuai <zhangjinshuai668@126.com>
2018-04-10 09:32:57 +0800

    ok

---
+++ modules/queue/controllers/QueueController.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zhangshuai
+ * Date: 18-3-31
+ * Time: 下午6:45
+ */
+
+namespace app\modules\queue\controllers;
+
+
+use app\controllers\BaseController;
+
+class QueueController extends BaseController
+{
+    public function actionIndex()
+    {
+
+    }
+}
