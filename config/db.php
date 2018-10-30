18316d25d909dcb940678678bec8adef6f44d343
zhangjinshuai <zhangjinshuai668@126.com>
2018-10-30 19:09:38 +0800

    初始化

--- config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=127.0.0.1;dbname=yii2hd',
-    'username' => 'root',
-    'password' => '55743011',
-    'charset' => 'utf8',
-];
